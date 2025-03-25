44f4459d8548a4d25683188d830ee0ca1cead22f
Charlie Clark <cr-clark@outlook.com>
2025-03-25 19:18:48 -0400

    Copied original model

--- scripts/modeling/models/temporal_models.py
+++ scripts/modeling/models/temporal_models.py
@@ -3,6 +3,46 @@ from typing import List
 import torch.nn as nn
 import torch
 
+class OGTemporalNet(nn.Module):
+    def __init__(self, input_size: int, output_size: int, hidden_sizes: List[int]):
+        '''
+        A simple MLP stack to serve as a starting point for our post-processing temporal network.
+
+        Inputs:
+            input_size: the size of the first input layer.
+            output_size: the size of the final output layer.
+            hidden_sizes: a list of sizes for the intermediate hidden layers.
+        '''
+
+        super().__init__()
+        self.__version__ = '0.0.1'
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+        self.layers = nn.ModuleList()
+
+        self.layers.append(nn.Linear(input_size, hidden_sizes[0]))
+        for i in range(1, len(hidden_sizes)):
+            self.layers.append(nn.Linear(hidden_sizes[i - 1], hidden_sizes[i]))
+        self.layers.append(nn.Linear(hidden_sizes[-1], output_size))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        '''
+        Performs a forward pass of the model defined in __init__.
+
+        Input:
+            x: a Tensor containing temporal data.
+
+        Output:
+            x: a Tensor containing the output of the final linear activation layer, after running the input x through the entire model.
+        '''
+
+        for layer in self.layers:
+            x = layer(x)
+
+        return x
+    
 class TemporalNet(nn.Module):
     def __init__(self, input_size: int, output_size: int, hidden_sizes: List[int], dropout: float = 0.3):
         '''
